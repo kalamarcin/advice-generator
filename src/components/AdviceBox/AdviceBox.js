3d01941037af763823d8db0a1a309750129a1e0a
Marcin Kala <kalamarcin1@gmail.com>
2022-12-17 12:49:03 +0100

    style done

--- src/components/AdviceBox/AdviceBox.js
+++ src/components/AdviceBox/AdviceBox.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import './AdviceBox.scss'
+import divider from '../../assets/images/pattern-divider-mobile.svg'
+import dice from '../../assets/images/icon-dice.svg'
 
 const AdviceBox = () => {
   const [advice, setAdvice] = useState('')
@@ -9,31 +11,35 @@ const AdviceBox = () => {
   const fetchData = () => {
     const API = 'https://api.adviceslip.com/advice'
     axios
-    .get(API)
-    .then(res => {
-      const getAdviceId = res.data.slip.id
-      const getAdvice = res.data.slip.advice
+      .get(API)
+      .then(res => {
+        const getAdviceId = res.data.slip.id
+        const getAdvice = res.data.slip.advice
 
-      setAdvice(getAdvice)
-      setAdviceId(getAdviceId)
-    })
-    .catch (
-      err => {
+        setAdvice(getAdvice)
+        setAdviceId(getAdviceId)
+      })
+      .catch(err => {
         console.log(err)
-      }
-    )
+      })
   }
 
   useEffect(() => {
-    // fetchData()
+    fetchData()
   }, [])
 
   return (
     <section className="box">
-      <div className="box__id">{adviceId}</div>
-      <div className="box__title">{advice}</div>
-
-      <button onClick={fetchData}> Click</button>
+      <div className="box__id">ADVICE #{adviceId}</div>
+      <div className="box__title">
+      "{advice}"
+      </div>
+      <div className="box__divider">
+        <img src={divider} alt="divider line" />
+      </div>
+      <div className="box__circle">
+        <button onClick={fetchData}><img src={dice} alt="" /></button>
+      </div>
     </section>
   )
 }
